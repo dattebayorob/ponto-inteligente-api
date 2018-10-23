7f4532dda75d883acfd633caa8382f81b7bc84b8
dattebayoRob <robson.william65@gmail.com>
2018-10-23 16:42:39 -0300

    changes in repositorys

--- src/main/java/com/dtb/pontointeligente/api/model/repositories/EmpresaRepository.java
+++ src/main/java/com/dtb/pontointeligente/api/model/repositories/EmpresaRepository.java
@@ -1,9 +1,12 @@
 package com.dtb.pontointeligente.api.model.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dtb.pontointeligente.api.model.entities.Empresa;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
-
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
 }
--- src/main/java/com/dtb/pontointeligente/api/model/repositories/FuncionarioRepository.java
+++ src/main/java/com/dtb/pontointeligente/api/model/repositories/FuncionarioRepository.java
@@ -1,9 +1,14 @@
 package com.dtb.pontointeligente.api.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dtb.pontointeligente.api.model.entities.Funcionario;
 
+@Transactional(readOnly= true)
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
-
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	Funcionario findByCpfOrEmail(String cpf,String email);
 }
