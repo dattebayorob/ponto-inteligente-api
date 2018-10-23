b1c230aaceb6f0fbcdf1fd0f258062d709347e11
dattebayoRob <robson.william65@gmail.com>
2018-10-23 15:40:49 -0300

    Method to encode strings

---
+++ src/main/java/com/dtb/pontointeligente/api/model/security/utils/PasswordUtils.java
@@ -0,0 +1,20 @@
+package com.dtb.pontointeligente.api.model.security.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	public PasswordUtils() {
+		// TODO Auto-generated constructor stub
+	}
+	public static String gerarBCrypt(String senha) {
+		if (senha.isEmpty()) {
+			return senha;
+		}
+		log.info("Gerando hash");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(senha);
+	}
+}
